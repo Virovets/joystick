66fa0b1f5f41f9f79f2d49f476214da1e222c376
Anton Virovets <antonvirovets@gmail.com>
2023-09-18 13:16:38 +0300

    new separate фів

--- script.js
+++ script.js
@@ -1,74 +1,42 @@
-class Joystick {
-    constructor(joystickElement, stickElement) {
-        this.joystickElement = joystickElement;
-        this.stickElement = stickElement;
-        this.isActive = false;
-        this.initialPositionX = 0;
-        this.initialPositionY = 0;
-        this.cursorOffsetX = 0;
-        this.cursorOffsetY = 0;
+const joystick1 = document.getElementById('joystick1');
+const joystick2 = document.getElementById('joystick2');
 
-        this.stickElement.addEventListener('touchstart', (e) => this.onTouchStart(e));
-        document.addEventListener('touchmove', (e) => this.onTouchMove(e));
-        document.addEventListener('touchend', () => this.onTouchEnd());
-    }
+joystick1.addEventListener('touchstart', (e) => {
+    e.preventDefault();
+    document.addEventListener('touchmove', moveJoystick1);
+    document.addEventListener('touchend', () => {
+        document.removeEventListener('touchmove', moveJoystick1);
+    });
+});
 
-    onTouchStart(e) {
-        e.preventDefault();
-        this.isActive = true;
-        this.stickElement.style.transition = '0s';
-        const joystickRect = this.joystickElement.getBoundingClientRect();
-        const touch = e.touches[0];
-        this.initialPositionX = touch.clientX - joystickRect.left;
-        this.initialPositionY = touch.clientY - joystickRect.top;
-        this.cursorOffsetX = this.stickElement.offsetWidth / 2;
-        this.cursorOffsetY = this.stickElement.offsetHeight / 2;
-    }
+joystick2.addEventListener('touchstart', (e) => {
+    e.preventDefault();
+    document.addEventListener('touchmove', moveJoystick2);
+    document.addEventListener('touchend', () => {
+        document.removeEventListener('touchmove', moveJoystick2);
+    });
+});
 
-    onTouchMove(e) {
-        if (this.isActive) {
-            const touch = e.touches[0];
-            const joystickRect = this.joystickElement.getBoundingClientRect();
-            this.moveStick(
-                touch,
-                joystickRect,
-                this.stickElement,
-                this.cursorOffsetX,
-                this.cursorOffsetY,
-                this.initialPositionX,
-                this.initialPositionY
-            );
-        }
-    }
+function moveJoystick1(e) {
+    const rect = joystick1.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+    const touch = e.touches[0];
+    const angle = Math.atan2(touch.clientY - centerY, touch.clientX - centerX);
+    const distance = Math.min(rect.width / 2, Math.hypot(touch.clientX - centerX, touch.clientY - centerY));
 
-    onTouchEnd() {
-        this.isActive = false;
-        this.stickElement.style.transition = '0.2s';
-        this.resetStickPosition(this.stickElement);
-    }
-
-    moveStick(touch, joystickRect, stickElement, cursorOffsetX, cursorOffsetY, initialPositionX, initialPositionY) {
-        const x = touch.clientX - joystickRect.left - cursorOffsetX - initialPositionX;
-        const y = touch.clientY - joystickRect.top - cursorOffsetY - initialPositionY;
-
-        const stickRadius = stickElement.offsetWidth / 2;
-        const maxDistance = joystickRect.width / 2 - stickRadius + 30;
-
-        const distance = Math.sqrt(x * x + y * y);
-        if (distance <= maxDistance) {
-            stickElement.style.transform = `translate(${x - 32}px, ${y - 32}px)`;
-        } else {
-            const angle = Math.atan2(y, x);
-            const newX = Math.cos(angle) * maxDistance;
-            const newY = Math.sin(angle) * maxDistance;
-            stickElement.style.transform = `translate(${newX - 32}px, ${newY - 32}px)`;
-        }
-    }
-
-    resetStickPosition(stickElement) {
-        stickElement.style.transform = 'translate(-50%, -50%)';
-    }
+    const stick = joystick1.querySelector('.stick');
+    stick.style.transform = `translate(-50%, -50%) translate(${Math.cos(angle) * distance}px, ${Math.sin(angle) * distance}px)`;
 }
 
-const joystick1 = new Joystick(document.getElementById('joystick1'), document.getElementById('stick1'));
-const joystick2 = new Joystick(document.getElementById('joystick2'), document.getElementById('stick2'));
+function moveJoystick2(e) {
+    const rect = joystick2.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+    const touch = e.touches[0];
+    const angle = Math.atan2(touch.clientY - centerY, touch.clientX - centerX);
+    const distance = Math.min(rect.width / 2, Math.hypot(touch.clientX - centerX, touch.clientY - centerY));
+
+    const stick = joystick2.querySelector('.stick');
+    stick.style.transform = `translate(-50%, -50%) translate(${Math.cos(angle) * distance}px, ${Math.sin(angle) * distance}px)`;
+}
